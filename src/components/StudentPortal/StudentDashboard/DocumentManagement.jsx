0203b0983350295a4afacf600f4decbd89f4fc94
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2024-07-28 00:01:33 +0100

    First Commit

--- src/components/StudentPortal/StudentDashboard/DocumentManagement.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DocumentManagement = () => {
-  return <div>Document Management Content</div>;
-};
-
-export default DocumentManagement;
--- src/components/StudentPortal/StudentDashboard/Help.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Help = () => {
-  return <div>Help Content</div>;
-};
-
-export default Help;
