0dd0bcf342f406ab2cf5b7721c13f667e9ddf40c
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2025-11-09 22:49:02 +0100

    update ui

--- src/components/StudentPortal/login/Login.jsx
+++ src/components/StudentPortal/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef, FormEvent } from "react";
+import { useState, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useSession } from "../../../context/session";
